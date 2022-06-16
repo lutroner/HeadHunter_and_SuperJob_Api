dd0c9459203d4d6f60d6c74ae1a8d2ba2df88163
lutroner <102256437+lutroner@users.noreply.github.com>
2022-06-17 01:25:16 +0300

    Initial commit

--- main.py
+++ main.py
@@ -1,16 +1,35 @@
-# This is a sample Python script.
+import requests
+from pprint import pprint
+import environs
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+HH_BASE_URL = 'https://api.hh.ru/vacancies'
+PROGRAMMING_CATEGORY_ID = 96
+SEARCH_PERIOD = 30
+AREA_ID = 1
+PROGRAMMING_LANGUAGES = ('Python', 'Java', 'Perl', 'JavaScript', 'C++', 'C#', 'Go', 'Ruby', 'Php', 'Rust')
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def predict_rub_salary(vacancy):
+    pass
 
 
-# Press the green button in the gutter to run the script.
+def get_headhunter_vacancies():
+    vacancies = {}
+    for language in PROGRAMMING_LANGUAGES:
+        payload = {'professional_role': PROGRAMMING_CATEGORY_ID,
+                   'period': SEARCH_PERIOD,
+                   'area': AREA_ID,
+                   'text': language
+                   }
+        response = requests.get(HH_BASE_URL, params=payload)
+        response.raise_for_status()
+        vacancies[language] = response.json()['found']
+    return vacancies
+
+
+def main():
+    pprint(get_headhunter_vacancies())
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    main()
